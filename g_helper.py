37465801eb8b5d0a889a6e5c154cbffd58cb0ec7
timpinoy <tim.pinoy@waeren.net>
2024-04-05 21:55:03 +0200

    First commit

---
+++ g_helper.py
@@ -0,0 +1,75 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window:
+    def __init__(self, width, height):
+        self.__root = Tk()
+        self.__root.title("MazeSolver")
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+        self.__canvas = Canvas(self.__root,
+                               width=width,
+                               height=height)
+        self.__canvas.pack()
+        self.__is_running = False
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wait_for_close(self):
+        self.__is_running = True
+        while self.__is_running:
+            self.redraw()
+
+    def close(self):
+        self.__is_running = False
+        
+    def draw_line(self, line, fill_color):
+        line.draw(self.__canvas, fill_color)
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Line:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+    def draw(self, canvas, fill_color):
+        canvas.create_line(self.p1.x, self.p1.y,
+                           self.p2.x, self.p2.y,
+                           fill=fill_color, width=2)
+
+
+class Cell:
+    def __init__(self, window):
+        self.__window = window
+        self.left = True
+        self.right = True
+        self.top = True
+        self.bottom = True
+        self.__x1 = None
+        self.__y1 = None
+        self.__x2 = None
+        self.__y2 = None
+
+    def draw(self, x1, y1, x2, y2):
+        self.__x1 = x1
+        self.__y1 = y1
+        self.__x2 = x2
+        self.__y2 = y2
+        if self.left:
+            line = Line(Point(self.__x1,self.__y1), Point(self.__x1,self.__y2))
+            self.__window.draw_line(line, "red")
+        if self.right:
+            line = Line(Point(self.__x2,self.__y1), Point(self.__x2,self.__y2))
+            self.__window.draw_line(line, "red")
+        if self.top:
+            line = Line(Point(self.__x1,self.__y1), Point(self.__x2,self.__y1))
+            self.__window.draw_line(line, "red")
+        if self.bottom:
+            line = Line(Point(self.__x1,self.__y2), Point(self.__x2,self.__y2))
+            self.__window.draw_line(line, "red")
---
+++ main.py
@@ -0,0 +1,18 @@
+from g_helper import *
+
+def main():
+    win = Window(800, 600)
+    
+    cell1 = Cell(win)
+    cell1.draw(20, 20, 40, 40)
+    cell2 = Cell(win)
+    cell2.right = False
+    cell2.draw(60, 20, 100, 40)
+    cell3 = Cell(win)
+    cell3.bottom = False
+    cell3.draw(20, 60, 40, 80)
+
+    win.wait_for_close()
+
+
+main()
